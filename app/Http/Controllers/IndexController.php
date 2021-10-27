1e951fdedc31cf507ba1a18fb402a53fd5198868
audreytilanov <tilanovaudrey@gmail.com>
2021-10-27 23:51:27 +0800

    -

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\About;
+
 
 class IndexController extends Controller
 {
-    //
+    public function about(){
+        $all = About::all();
+
+        return view('')->with(compact('all'));
+    }
 }
